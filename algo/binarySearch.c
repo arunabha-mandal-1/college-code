924c5242f0caddf05bf5e341c46716ba95dc79b5
Arunabha Mandal <arunabhamandal461@gmail.com>
2022-07-01 12:20:14 +0530

    binary search added

---
+++ algo/binarySearch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int binarySearch(int [], int, int, int);
+int main(){
+    int arr[6] = {2, 1, 4, 3, 5, 9};
+    int flag = binarySearch(arr, 0, 5, 9);
+    if(flag==-1)
+        printf("Not found.\n");
+    else
+        printf("Found at %d index.", flag);
+    return 0;
+}
+
+int binarySearch(int arr[], int start, int end, int element){
+    int mid = start + (end - start) / 2;
+    if(start > end)
+        return -1;
+    if(arr[mid]==element)
+        return mid;
+    else if(element < arr[mid])
+        return binarySearch(arr, start, mid-1, element);
+    else if(element > arr[mid])
+        return binarySearch(arr, mid+1, end, element);
+    // return -1;
+}
+
+// https://www.geeksforgeeks.org/binary-search/
